969c5491f5648772222a0b73477f15d8b4e40f41
M. Kaan SARICA <kaanmehmet2561@gmail.com>
2024-06-03 16:50:59 +0300

    03.06.2024

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
 
 namespace MyPortfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult ExperienceList()
-        {  
+        {
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+                context.Experiences.Add(experience); // gelen veriyi al
+                context.SaveChanges(); // gelen veriyi kaydet
+                return RedirectToAction("ExperienceList"); // kaydedilen veriyi experience liste gönder
+      
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            context.Experiences.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            context.Experiences.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
--- MyPortfolio/DAL/Entities/Experience.cs
+++ MyPortfolio/DAL/Entities/Experience.cs
@@ -5,7 +5,7 @@
         public int ExperienceId { get; set; }
         public string Head { get; set; }
         public string Title { get; set; }
-        public string Date { get; set; }
+        public string? Date { get; set; }
         public string Description { get; set; }
     }
 }
